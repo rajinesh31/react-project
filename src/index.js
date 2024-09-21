72e4d51dc8432d9a6799bf89549af2ee051ad903
rajinesh31 <rajinesh31@gmail.com>
2024-09-22 00:29:30 +0530

    code changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GameBoard from './Components/GameBoard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GameBoard />
   </React.StrictMode>
 );
 
