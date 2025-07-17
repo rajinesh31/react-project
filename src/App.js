36a365153a1982ddb5d8e72631d0718608a14ba0
rajinesh31 <146704256+rajinesh31@users.noreply.github.com>
2025-07-16 23:45:37 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Main from './main';
+import Footer from './Footer';
+function App() {
+  return (
+    <div>
+            <Header />
+            <Main />
+            <Footer/>
+        </div>
+  );
+}
+
+export default App;
---
+++ src/componends/main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+    <div>
+       <h2>main</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid deleniti qui accusamus minima porro vitae laborum totam similique consectetur! Dolores unde quibusdam optio voluptatem in culpa recusandae et ullam quia.</p>
+    </div>
+    )
+;};
+export default Main;
