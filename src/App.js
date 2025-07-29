93fc891b35ac64a7aca9083161c598b09691ccb0
Rajinesh Chennoju <rajinesh31@gmail.com>
2025-07-29 23:59:39 +0530

    index.js removed from the folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import Main from './main';
-import Footer from './Footer';
+import Header from './components/Header';
+import Main from './components/main';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
@@ -13,3 +13,34 @@ function App() {
 }
 
 export default App;
+// import React from 'react';
+// import Header from './Header';
+// import Main from './main';
+// import Footer from './Footer';
+// import Welcome from './Welcome';
+// const App = () => {
+//     return (
+//         <div>
+//             <Header />
+//             <Main />
+//             <Footer/>
+//             <Welcome />
+//         </div>
+//     );
+// };
+// export default App;
+// const App = () => {
+//     const myname = "rajinesh"
+//     const multipy = (a,b) => a*b
+//     const specialclass = 'simple'
+//     return (
+//         <section id="section">
+//             <p>{myname}</p>
+//             <p>2 + 2 = {2 + 2}</p>
+//             <p>2*2={multipy(2,2)}</p>
+//             <p className={specialclass}>This is spicale</p>
+//         </section>
+        
+//     );
+// };
+// export default App;
